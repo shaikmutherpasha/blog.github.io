18a807d77f7ffa7760fa04a627a18e867838aa63
shaikmutherpasha <shaikmutherpasha@users.noreply.github.com>
2016-12-18 23:43:19 +0530

    added js

---
+++ script.js
@@ -0,0 +1,13 @@
+$.ajax({
+        method: 'GET',
+        url:'http://jsonplaceholder.typicode.com/posts',
+        datType: 'json'
+    }).done(function(data){
+        $.map(data,function(post, i){
+          $('.pagination__list').append("<div class='panel panel-primary pagination__item'><div class='panel-heading'>"
+       + post.title + "<div class='ed-icons'>"+
+       "<span class='glyphicon glyphicon-edit' title='Edit Post'></span> |"+
+       " <span class='glyphicon glyphicon-trash' title='Delete Post'></span></div>"+
+       "</div><div class='panel-body'>" + post.body + "</div></div>")
+        })
+    })
