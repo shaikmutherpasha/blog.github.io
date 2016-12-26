5c9828ed720c7d1a17d8e54aa9734d1bb32554ff
shaikmutherpasha <shaikmutherpasha@users.noreply.github.com>
2016-12-26 15:29:10 +0530

    add "post"

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 $.ajax({
-        method: 'GET',
-        url:'http://jsonplaceholder.typicode.com/posts',
-        dataType: 'json'
-    }).done(function(data){
-        $.map(data,function(post, i){
-          $('.pagination__list').append("<div class='panel panel-primary pagination__item'><div class='panel-heading'>"
-       + post.title + "<div class='ed-icons'>"+
-       "<span class='glyphicon glyphicon-edit' title='Edit Post'></span> |"+
-       " <span class='glyphicon glyphicon-trash' title='Delete Post'></span></div>"+
-       "</div><div class='panel-body'>" + post.body + "</div></div>")
-        })
+    method: 'GET',
+    url: 'http://jsonplaceholder.typicode.com/posts',
+    datType: 'json'
+}).done(function(data) {
+    $.map(data, function(post, i) {
+        $('#panels').append("<div class='panel panel-primary'><div class='panel-heading'>" + post.title + "<div class='ed-icons'>" +
+            "<span class='glyphicon glyphicon-edit' title='Edit Post'></span> |" +
+            " <span class='glyphicon glyphicon-trash' title='Delete Post'></span></div>" +
+            "</div><div class='panel-body'>" + post.body + "</div></div>")
     })
+})
+$('form').submit(function(e){
+    e.preventDefault();
+    var title = $('#title').val();
+    var body = $('#desc').val();
+    var url = 'http://jsonplaceholder.typicode.com/posts';
+
+    $.post(url, {title:title,body:body}).done(function(post){
+       $('#panels').prepend("<div class='panel panel-primary'><div class='panel-heading'>" + post.title + "<div class='ed-icons'>" +
+            "<span class='glyphicon glyphicon-edit' title='Edit Post'></span> |" +
+            " <span class='glyphicon glyphicon-trash' title='Delete Post'></span></div>" +
+            "</div><div class='panel-body'>" + post.body + "</div></div>")
+    })
+
+})
+
+ $('#submit').click(function(){
+     $('.modal').fadeOut();
+     $('.modal-backdrop').fadeOut();
+   });
